24b4b9a33895cc25663b60e2a13f98bab9ae18a8
emarketingcontacto <emarketing.contacto@gmail.com>
2024-03-21 18:38:13 -0600

    Title Layout

--- resources/views/Material/edit.blade.php
+++ resources/views/Material/edit.blade.php
@@ -1,33 +1,32 @@
-@extends('layout')
+{{-- @extends('layout') --}}
+<x-layout pageTitle="Management">
 
-@section('main-content')
+    @section('main-content')
 
-<h1>Edit Material</h1>
+    <h1>Edit Material</h1>
+    <div class="container">
+        <form method="POST" action="{{route('Material.update',['material' => $material])}}")}}">
+            @csrf
+            @method('put')
+            <div class="mb-3">
+            <label for="materialId" class="form-label">ID</label>
+            <input type="number" name="materialId" class="form-control w-25" disabled value="{{$material->materialId}}">
+            </div>
 
-<div class="container">
-    <form method="POST" action="{{route('Material.update',['material' => $material])}}")}}">
-        @csrf
-        @method('put')
-        <div class="mb-3">
-          <label for="materialId" class="form-label">ID</label>
-          <input type="number" name="materialId" class="form-control w-25" disabled value="{{$material->materialId}}">
-        </div>
+            <div class="mb-3">
+            <label for="materialName" class="form-label">Name</label>
+            <input type="text"  name="materialName" class="form-control w-50" value="{{$material->materialName}}"">
+            {{-- Error --}}
+            @error('materialName')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            </div>
 
-        <div class="mb-3">
-          <label for="materialName" class="form-label">Name</label>
-          <input type="text"  name="materialName" class="form-control w-50" value="{{$material->materialName}}"">
-        {{-- Error --}}
-        @error('materialName')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-        </div>
+            <button type="submit" class="btn btn-sm btn-primary">Save</button>
+            <a href="/Material" class="btn btn-sm btn-danger">Cancel</a>
+        </form>
+    </div>
+    @endsection
 
-        <button type="submit" class="btn btn-sm btn-primary">Save</button>
-        <a href="/Material" class="btn btn-sm btn-danger">Cancel</a>
-    </form>
-</div>
-
-
-@endsection
-
+</x-layout>
 
