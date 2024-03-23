3fbafc7743808d342d99c1209e6f656d8afc6b41
emarketingcontacto <emarketing.contacto@gmail.com>
2024-03-22 19:49:08 -0600

    H1 Subcategories

--- resources/views/Subcategories.blade.php
+++ resources/views/Subcategories.blade.php
@@ -4,28 +4,33 @@
 <x-layout pageTitle="Venta de zapato {{$subcategory}}"  pageDescription={{$description}}>
 
     @section('main-content')
+
+    <div class="subcatTitle d-flex justify-content-center align-items-center mt-5 pt-5" style="width:100vw; height:50vh">
+        <h1>Catálogo de Zapatos {{$subcategory}}</h1>
+    </div>
+
     <div class="d-flex mt-5 mb-5 pb-5 gap-3 flex-wrap justify-content-center">
-            @foreach ($models as $model )
+
+        @foreach ($models as $model )
+
             <div class="card" style="width: 18rem;">
-                <img src="{{asset('storage/'.$model->modelImage)}}" class="card-img-top" height="350px" alt="...">
-                <div class="card-body">
-                    <a href="{{route('Modelo.show', ['modelo'=>$model->modelId])}}" class="btn btn-sm btn-show form-control">
+
+                <img src="{{asset('storage/'.$model->modelImage)}}" class="card-img-top" height="350px" alt="{{$model->bizName}}">
+
+                <div>
+                    <a href="{{route('Modelo.show', ['modelo'=>$model->modelId])}}">
                         <h5 class="card-title">{{$model->modelName}}</h5>
                     </a>
-
-                  <p class="card-text">{{$model->saletypeConditions}}</p>
                 </div>
+
                 <ul class="list-group list-group-flush text-center">
+                  <li class="list-group-item">{{$model->saletypeConditions}}</li>
                   <li class="list-group-item">{{$model->modelsubcatName}}</li>
                   <li class="list-group-item">{{$model->modelPrice}}.00</li>
                   <li class="list-group-item">{{$model->bizName}}</li>
                 </ul>
-                <div class="card-body mt-3">
-                    <div class="btn-show">
-                      <a href="{{route('Modelo.show', ['modelo'=>$model->modelId])}}" class="btn btn-sm btn-show form-control">{{$model->modelName}}</a>
-                    </div>
-                </div>
-              </div>
+            </div>
+
             @endforeach
     </div>
 @endsection
