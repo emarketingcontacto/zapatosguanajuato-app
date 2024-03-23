b0c8d03d7a4d1c3cf4d578676100826c387056e7
emarketingcontacto <emarketing.contacto@gmail.com>
2024-03-22 21:52:03 -0600

    H1Modelo

--- resources/views/Register.blade.php
+++ resources/views/Register.blade.php
@@ -3,6 +3,8 @@
 
 
 @section('main-content')
+<div class="payments mb-5 pb-5">
+
   {{-- PAYPAL TEXT --}}
             <div class="paypal pt-5">
                 <span class="pt-5">
@@ -13,39 +15,55 @@
                         acceso a los recursos más valiosos para tu negocio!
                     </p>
                     <h2 class="text-center"> Acceso ilimitado por todo un año</h2>
-                    <h1 class="text-center"> $199.00 MXN</h1>
+                    <h1 class="text-center"> $499.00 MXN</h1>
                 </span>
-
             </div>
             {{-- END PAYPAL TEXT --}}
 
-{{-- Paypal Buttons--}}
-<div class="paypalbuttons">
+            {{-- Paypal Buttons--}}
+            {{-- <div class="paypalbuttons">
+                <div id="paypal-button-container-P-70396561M81280630MX4NJZY"></div>
+            </div> --}}
+            {{-- EndPaypal Buttons--}}
 
-    <div id="paypal-button-container-P-70396561M81280630MX4NJZY"></div>
-    <script src="https://www.paypal.com/sdk/js?client-id=AbIQh2FbR_tDfsZYfD72KzDN10u-ol5aXjhnx5WoNG1tEWjYDZFvrYWBv-E2IpLzYU4qWO7ZWTD-GBFL&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
-    <script>
-      paypal.Buttons({
-          style: {
-              shape: 'rect',
-              color: 'blue',
-              layout: 'vertical',
-              label: 'subscribe'
-          },
-          createSubscription: function(data, actions) {
-            return actions.subscription.create({
-              /* Creates the subscription */
-              plan_id: 'P-70396561M81280630MX4NJZY'
-            });
-          },
-          onApprove: function(data, actions) {
-            alert(data.subscriptionID); // You can add optional success message for the subscriber here
-          }
-      }).render('#paypal-button-container-P-70396561M81280630MX4NJZY'); // Renders the PayPal button
-    </script>
+            {{-- card-payments --}}
+            <div class="card-payments d-flex justify-content-center align-items-center" style="width:100vw; heigth:50vh">
+                    {{-- <div class="buttons-payments d-flex justify-content-center align-items-center" style="50vw" >
+                        <a href="https://buy.stripe.com/5kA3d62t280R10IfYY" class="btn btn-primary btn-lg form-control-lg" style="width: 50vw">
+                            <i class="bi bi-credit-card fs-1 pt-3px" ></i>
+                            Realizar Pago Con Tarjeta</a>
+                    </div> --}}
 
+                    <div class="buttons-payments d-flex justify-content-center align-items-center bg-gray-300" style="50vw">
+                        <script async src="https://js.stripe.com/v3/buy-button.js"></script>
+                        <stripe-buy-button buy-button-id="buy_btn_1OxL2JRxi1sURpG4SVohhhBb" publishable-key="pk_live_51OwDydRxi1sURpG4V3heOuFCAtE2xUaALTlVWH5bYLxaYbT1d9cAVTIXjgDgJdZ25MdjjUM5ox9tj05JIMW5t13R00fbqanJhV">
+                        </stripe-buy-button>
+                    </div>
+
+            </div>
+            {{-- card-payments --}}
 </div>
-{{-- EndPaypal Buttons--}}
+
+<script src="https://www.paypal.com/sdk/js?client-id=AbIQh2FbR_tDfsZYfD72KzDN10u-ol5aXjhnx5WoNG1tEWjYDZFvrYWBv-E2IpLzYU4qWO7ZWTD-GBFL&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
+<script>
+paypal.Buttons({
+    style: {
+        shape: 'rect',
+        color: 'blue',
+        layout: 'vertical',
+        label: 'subscribe'
+    },
+    createSubscription: function(data, actions) {
+        return actions.subscription.create({
+        /* Creates the subscription */
+        plan_id: 'P-70396561M81280630MX4NJZY'
+        });
+    },
+    onApprove: function(data, actions) {
+        alert(data.subscriptionID); // You can add optional success message for the subscriber here
+    }
+}).render('#paypal-button-container-P-70396561M81280630MX4NJZY'); // Renders the PayPal button
+</script>
 
 @endsection
 
