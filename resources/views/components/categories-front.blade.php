5a78bf30442123cf4e4c9800312f4bac1450f41e
emarketingcontacto <emarketing.contacto@gmail.com>
2025-02-19 16:01:27 -0600

    h4 categories

--- resources/views/components/categories-front.blade.php
+++ resources/views/components/categories-front.blade.php
@@ -7,19 +7,19 @@
     <div class="cat-front">
         <a href="{{route('Fabricantes')}}">
             <i class="bi bi-building-fill-gear"></i>
-            <h5>Fabricantes</h5>
+            <h4>Fabricantes</h4>
         </a>
     </div>
     <div class="cat-front">
         <a href="{{route('Wholesalers')}}">
             <i class="bi bi-houses-fill"></i>
-            <h5>Mayoristas</h5>
+            <h4>Mayoristas</h4>
         </a>
     </div>
     <div class="cat-front">
         <a href="{{route('Retails')}}">
             <i class="bi bi-bag-plus-fill"></i>
-            <h5>Menudistas</h5>
+            <h4>Menudistas</h4>
         </a>
     </div>
 </div>
