6ec18fca1178f8e17d33902de86e548d67ed397e
emarketingcontacto <emarketing.contacto@gmail.com>
2025-03-13 18:40:11 -0600

    defer js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,11 +27,14 @@
         {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
         {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> --}}
         {{-- local --}}
-        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css')}}">
-        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" />
+        {{-- <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}" /> --}}
         {{-- online --}}
-        {{-- <link rel="stylesheet" href="{{ URL::asset('css/main-min.css')}}">
-        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-min.css')}}"> --}}
+        {{-- <link rel="preload" as="style" href="{{ URL::asset('css/main-min.css')}}" onload="this.onload=null;this.rel='stylesheet'">
+        <link rel="preload" as="style" href="{{ URL::asset('css/bootstrap-min.css')}}" onload="this.onload=null;this.rel='stylesheet'"> --}}
+
+        <link rel="stylesheet" href="{{ URL::asset('css/main-min.css')}}">
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap-min.css')}}">
     {{-- style --}}
 
     {{-- sripts local --}}
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -25,14 +25,14 @@
         </div>
         <div class="col footer-socials">
             <div>
-                <a href="https://solucion-e.com.mx/">
+                <a href="https://solucion-e.com.mx/" aria-label="solucion-e.com.mx">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-browser-chrome" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M16 8a8 8 0 0 1-7.022 7.94l1.902-7.098a3 3 0 0 0 .05-1.492A3 3 0 0 0 10.237 6h5.511A8 8 0 0 1 16 8M0 8a8 8 0 0 0 7.927 8l1.426-5.321a3 3 0 0 1-.723.255 3 3 0 0 1-1.743-.147 3 3 0 0 1-1.043-.7L.633 4.876A8 8 0 0 0 0 8m5.004-.167L1.108 3.936A8.003 8.003 0 0 1 15.418 5H8.066a3 3 0 0 0-1.252.243 2.99 2.99 0 0 0-1.81 2.59M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
                       </svg>
                 </a>
             </div>
             <div>
-              <a href="https://www.facebook.com/zapatosguanajuatomx/">
+              <a href="https://www.facebook.com/zapatosguanajuatomx/" aria-label="facebook">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                   </svg>
@@ -40,21 +40,21 @@
 
             </div>
             <div>
-              <a href=" https://wa.me/4771598183">
+              <a href=" https://wa.me/4771598183" aria-label="whatsapp">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
                     <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
                   </svg>
               </a>
             </div>
             <div>
-              <a href="https://twitter.com/zapatosenleon">
+              <a href="https://twitter.com/zapatosenleon" aria-label="x">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
                     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                   </svg>
               </a>
             </div>
             <div>
-              <a href="mailto:admin@zapatosguanajuato.mx">
+              <a href="mailto:admin@zapatosguanajuato.mx" aria-label="mail">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
